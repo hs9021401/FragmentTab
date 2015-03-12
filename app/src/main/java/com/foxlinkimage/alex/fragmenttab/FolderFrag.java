52bfdb0869ce58b806690fb53ab3662938af05eb
hs9021401 <hs9021401@gmail.com>
2015-03-12 17:57:56 +0800

    20150312- Can List out the File
    Can List out the File

--- app/src/main/java/com/foxlinkimage/alex/fragmenttab/FolderFrag.java
+++ app/src/main/java/com/foxlinkimage/alex/fragmenttab/FolderFrag.java
@@ -1,23 +1,67 @@
 package com.foxlinkimage.alex.fragmenttab;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 2015/3/11.
  */
 public class FolderFrag extends Fragment {
+    public final static String folder_Path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath()+ "/MyPicFolder";
+    protected ListView fileList;
+
     public FolderFrag()
     {}
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //List all the file in the folder
+        ArrayList<String> FilesInFolder = GetFiles(folder_Path);
+        fileList = (ListView)getActivity().findViewById(R.id.fileList);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, FilesInFolder);
+        fileList.setAdapter(myAdapter);
+
+        fileList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), view.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_folder,container,false);
-        
+        View rootView = inflater.inflate(R.layout.fragment_folder, container, false);
         return rootView;
+    }
+
+    public ArrayList<String> GetFiles(String DirectoryPath)
+    {
+        ArrayList<String> MyFiles = new ArrayList<String>();
+        File f = new File(DirectoryPath);
+
+        f.mkdirs();
+        File[] files = f.listFiles();
+        if (files.length == 0)
+            return null;
+        else {
+            for (int i=0; i<files.length; i++)
+                MyFiles.add(files[i].getName());
+        }
+
+        return MyFiles;
     }
 }
--- app/src/main/java/com/foxlinkimage/alex/fragmenttab/MenuFragment.java
+++ app/src/main/java/com/foxlinkimage/alex/fragmenttab/MenuFragment.java
@@ -23,7 +23,7 @@ public class MenuFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.menu_multi,container,false);
+        View view = inflater.inflate(R.layout.fragment_menu,container,false);
 
         frag = new ScanFrag();
         fragmentTransaction = getFragmentManager().beginTransaction().add(R.id.container, frag);
